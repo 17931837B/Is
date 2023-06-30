35e93c9a1e41f4eab01e96a92e94ea09077fbee5
17931837B <136469835+17931837B@users.noreply.github.com>
2023-06-30 14:46:18 +0900

    Update Is.py

--- Is.py
+++ Is.py
@@ -1,7 +1,35 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-def capture_and_plot():
+def find_max_value_position(image_path):
+    # 画像を読み込む
+    img = cv2.imread(image_path)
+
+    # 画像の高さと幅を取得
+    height, width, _ = img.shape
+    print(height)
+    print(width)
+
+    # 各ピクセルごとに計算した値を得る
+    r = img[:,:,2].astype(int)
+    g = img[:,:,1].astype(int)
+    b = img[:,:,0].astype(int)
+    values = r*2 - (g + b)
+
+    # 最大値のインデックスを求める
+    max_index = np.unravel_index(np.argmax(values), values.shape)
+
+    print("最大値の位置（行, 列）:", height-max_index[0], width-max_index[1])
+    max_position = (max_index[1], max_index[0])
+
+    # 画像をプロットして最大値の位置に目印を追加
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.scatter(max_position[0], max_position[1], color='red', marker='x')
+    plt.axis('off')
+    plt.show()
+
+def capture_and_calculate():
     cap = cv2.VideoCapture(0)  # カメラをキャプチャする
 
     while True:
@@ -9,7 +37,7 @@ def capture_and_plot():
 
         cv2.imshow('Camera', frame)  # フレームを表示する
 
-        # Enterキーが押されたら画像を保存してプロットする
+        # Enterキーが押されたら画像を保存して計算する
         if cv2.waitKey(1) == 13:  # 13はEnterキーのキーコード
             cv2.imwrite('captured_image.jpg', frame)  # 画像を保存する
             break
@@ -17,12 +45,7 @@ def capture_and_plot():
     cap.release()  # カメラを解放する
     cv2.destroyAllWindows()  # ウィンドウを閉じる
 
-    # 保存した画像をプロットする
-    img = cv2.imread('captured_image.jpg')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
+    # 保存した画像の最大値の位置を求めてプロットする
+    find_max_value_position('captured_image.jpg')
 
-
-capture_and_plot()
+capture_and_calculate()
