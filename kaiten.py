6fbf23b902b24731004f5f7405ff2e4c6e62c73b
17931837B <136469835+17931837B@users.noreply.github.com>
2023-07-06 00:12:16 +0900

    Add files via upload

---
+++ kaiten.py
@@ -0,0 +1,19 @@
+import time
+import serial   #https://www.cooptec.jp/archives/1511
+
+# シリアル通信の設定
+ser = serial.Serial('/dev/ttyACM0', 9600)  # ポート名に応じて変更
+
+ser.flushInput()
+ser.flushOutput()
+rotate = True
+timer = 5   #何秒回転させるか、ここは実物みて距離計算するしかない。直径＊π　加速度も考えないと・・・
+timeS=time.time()
+NowTime = 0
+
+while NowTime>=timer:
+    ser.write(b's30\n')  # 角度設定(これで速さが変わるらしい)
+    NowTime = time.time()-timeS
+    
+    
+   
